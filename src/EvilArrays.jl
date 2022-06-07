b7cc7df45e36f7355d7de6edd669a0930e42097c
Colin Caine <cmcaine@gmail.com>
2022-06-07 16:30:30 +0100

    Implement in terms of OffsetArrays
    Fixes #3, closes #1, #2

--- src/EvilArrays.jl
+++ src/EvilArrays.jl
@@ -2,44 +2,9 @@ module EvilArrays
 
 export EvilArray
 
-import Base: size, length, getindex, eachindex, show, iterate, lastindex, keys, IndexStyle
+using OffsetArrays
 
-struct EvilArray{T, N, P<:AbstractArray{T, N}} <: AbstractArray{T, N}
-    a::P
+# Create a new offset array of the same size but with each axis starting at a random large negative offset.
+EvilArray(A) = OffsetArray(A, (rand(typemin(Int):typemin(Int)÷2) for _ in 1:ndims(A))...)
+
 end
-
-struct EvilArrayIndex{T}
-    i::T
-end
-
-## Methods documented to be part of the interface
-
-size(a::EvilArray) = size(a.a)
-
-getindex(a::EvilArray, inds...) = throw(KeyError(inds))
-getindex(a::EvilArray, inds::EvilArrayIndex...) = getindex(a.a, (i.i for i in inds)...)
-
-setindex!(a::EvilArray, v, inds...) = throw(KeyError(inds))
-setindex!(a::EvilArray, v, inds::EvilArrayIndex...) = setindex!(a.a, v, (i.i for i in inds)...)
-
-## Methods required to actually iterate the thing
-
-# IndexStyle doesn't work, dunno why.
-# IndexStyle(::Type{<:EvilArray{T, N, P}}) where {T, N, P} = IndexStyle(P)
-eachindex(a::EvilArray) = [EvilArrayIndex(i) for i in eachindex(a.a)]
-eachindex(::IndexLinear, a::EvilArray) = [EvilArrayIndex(i) for i in eachindex(IndexLinear(), a.a)]
-eachindex(::IndexCartesian, a::EvilArray) = [EvilArrayIndex(i) for i in eachindex(IndexCartesian(), a.a)]
-
-# default firstindex method works.
-lastindex(a::EvilArray) = EvilArrayIndex(lastindex(a.a))
-
-## Other methods that AbstractArray defines in ways incompatible with us
-
-keys(a::EvilArray) = eachindex(a)
-# broadcasting is broken too, idk how to fix.
-
-# AbstractArray show methods try to index at 1
-show(io::IO, a::EvilArray) = print(io, "EvilArray(", a.a, ")")
-show(io::IO, mime::MIME"text/plain", a::EvilArray) = print(io, "EvilArray(", a.a, ")")
-
-end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,31 @@ using Test
 @testset begin
     A = EvilArray([42])
 
-    @test_throws KeyError A[1]
+    # Indexing with 1 should break, so 1:length(A) will break.
+    @test_throws BoundsError A[1]
+    @test_throws BoundsError @inbounds A[1]
     @test collect(A) == [42]
 
     @test first(A) == 42
     @test A[firstindex(A)] == 42
     @test A[lastindex(A)] == 42
 
-    @test [A[k] for k in keys(A)] == [42]
+    @test first([A[k] for k in keys(A)]) == 42
 
-    # Don't know why broadcasting is broken and don't know how to fix it.
-    @test_broken EvilArray(1:10) .+ 1
+    # firstindex(A):lastindex(A) should be legit
+    A2 = EvilArray(5:10)
+    @test [A2[idx] for idx in firstindex(A2):lastindex(A2)] == 5:10
+    @test A2[begin:end] == 5:10
+
+    # Broadcasting works with a scalar
+    @test collect(EvilArray(1:10) .+ 1) == 2:11
+    # But not with an array with differing indices
+    @test_throws DimensionMismatch EvilArray(1:10) .+ (1:10)
+    @test_throws DimensionMismatch EvilArray(1:10) .+ EvilArray(1:10)
+    @test_throws DimensionMismatch EvilArray(1:10) .+ ones(10,10)
+    @test_throws DimensionMismatch EvilArray(1:10) .+ EvilArray(ones(10,10))
+
+    # If you use `similar` or `axes`, then broadcasting should work:
+    @test A2 .+ zeros(axes(A2)) == A2
+    @test (A2 .+ similar(A2); true) # testing that this does not throw an error
 end
